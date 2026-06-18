6d222dee7c265898a2963b3fd91279649ecfe297
NEXIFY-STUDIO <u0352652320@gmail.com>
2026-06-18 19:32:15 +0200

    chore(android): upgrade geocoding and compileSdk for release build (#9)
    * chore(android): upgrade geocoding for compileSdk 35
    
    * chore(android): bump compileSdk to 36
    
    ---------
    
    Co-authored-by: NEXIFY-STUDIO <tvoj-nexify-email@domain.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.goldtaxi.gold_taxi"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
